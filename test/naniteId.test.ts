db5329e46f28c4330e0a4bd6c49f5460ff0aaa3d
Enric Trenric_trillo <enric_trillo@outlook.com>
2022-10-27 19:19:57 +0100

    added regex check to ensure that the values are sliced correctly and don't start with the usual '0.'

--- test/naniteId.test.ts
+++ test/naniteId.test.ts
@@ -6,4 +6,9 @@ describe('nanite', () => {
         let _length = 10;
         expect(_id.length).toBeGreaterThanOrEqual(_length)
     })
+    it('no "0." at the start', () => {
+        let _id = naniteId();
+        let _regex = /^(0.)/gm;
+        expect(_id).not.toEqual(_regex)
+    });
 })
